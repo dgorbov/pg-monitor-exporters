368844e829121431a8340b14a957ce7064d49396
Dmitry Gorbov <4911304+dgorbov@users.noreply.github.com>
2020-01-06 12:48:15 +0300

    added region to backend to make terraform init without params

--- backend.tf
+++ backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "s3" {
+    region         = "eu-central-1"
     bucket         = "pg-monitor-exporters-tf-state"
     key            = "terraform/main-state"
     dynamodb_table = "tf-state-lock-pg-monitor-exporters-tf-state"
